f2ec964a8c0e351267bc401084aba9fba2b8fae8
Dmytro Ben <dmitriy.ben@gmail.com>
2021-05-09 23:25:32 +0300

    feat(client): added next.js, material-ui

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      main page
+      <Button>123</Button>
+    </div>
+  );
+}
+
+export default index;
