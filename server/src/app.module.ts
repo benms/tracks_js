aa9a95ad6f8dd9c4eae671b440b587cdb829138b
Dmytro Ben <dmitriy.ben@gmail.com>
2021-05-09 14:12:59 +0300

    feat(env): added support of environments

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,15 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackModule } from './track/track.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     TrackModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.y9sbb.mongodb.net/tracks_db?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
 })
 export class AppModule {}
