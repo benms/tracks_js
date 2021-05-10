ed5e7a947066ce8df2a9f6eec6215237179bedd1
Dmytro Ben <dmitriy.ben@gmail.com>
2021-05-10 14:09:36 +0300

    feat(client): adding nav bar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,24 @@
-import { Button } from '@material-ui/core'
 import React from 'react'
 
 const index = () => {
   return (
-    <div>
-      main page
-      <Button>123</Button>
+    <>
+    <div className="center">
+      <h1>Welcome</h1>
+      <h3>Best tracks</h3>
     </div>
+    <style jsx>
+      {`
+        .center {
+          margin-top: 150px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+      `}
+    </style>
+    </>
   );
 }
 
